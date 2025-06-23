325fe5aa6822b463fde7f9aac9c4f46c0482a5cc
Patel2080 <146932559+Patel2080@users.noreply.github.com>
2025-06-23 07:00:04 +0000

    datatype done

---
+++ 01_basics/datatype.js
@@ -0,0 +1,12 @@
+//number
+//bigint
+//strng=> ""
+// null => standalone value
+// boolean => true/false
+// undefined => not initialize
+// symbol => unique  /*The Symbol type is the set of all non-String values that may be used as the key of an Object property (6.1.7). Each possible Symbol value is unique and immutable.*/
+
+// object
+
+console.log(typeof undefined)  // output undefined
+console.log(typeof  null)      // output object
