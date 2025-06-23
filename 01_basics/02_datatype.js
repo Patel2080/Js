85ba49dc25a81970b7a0833b75dec92fc8e5a4ec
Patel2080 <146932559+Patel2080@users.noreply.github.com>
2025-06-23 09:25:18 +0000

    datatype summary

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -5,8 +5,22 @@
 // boolean => true/false
 // undefined => not initialize
 // symbol => unique  /*The Symbol type is the set of all non-String values that may be used as the key of an Object property (6.1.7). Each possible Symbol value is unique and immutable.*/
-
+   const id=Symbol('123');
+   const anotherId = Symbol('123');
+  
+   console.log( id == anotherId); //false
 // object
 
 console.log(typeof undefined)  // output undefined
 console.log(typeof  null)      // output object
+
+
+
+// DataType summary
+/* Types
+1. Primitive -> (String,Number,Boolean,null,undefined,symbol,BigInt)
+2. Reference(non primitive) -> (arrays,Objects, funtions)
+*/
+
+// JS is Dynamically Typed language bcs we do not define the variable type
+
