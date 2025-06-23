095fdf825239bda1627a8be814fef4172722d083
Patel2080 <146932559+Patel2080@users.noreply.github.com>
2025-06-23 09:06:19 +0000

    why string to number is confusing

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,10 @@
+console.log("2">1); //true
+console.log("02" >1); //true;
+
+console.log(null > 0);      //false
+console.log(null == 0);     //false
+console.log(null >= 0);     //true      (this  reason is that an equality check == and compparisions >,<,>=,<= work differently )
+
+console.log(undefined == 0);     //false
+console.log(undefined > 0);     //false
+console.log(undefined < 0);     //false
