d58c824fbededf6181e63683439aeb2d1387fdfb
Patel2080 <146932559+Patel2080@users.noreply.github.com>
2025-06-23 05:44:34 +0000

    Variable done

---
+++ 01_basics/variable.js
@@ -0,0 +1,16 @@
+const id=1;
+let name="Suraj"
+let age=12
+var course="B.Com"  /* prefer not to use
+var because of issue in block scope and funtional scope
+*/
+city="nagpur" 
+let placement;
+
+//id=2  //not allowed
+name="patel"
+age=21
+course="B.Tech"
+city="Kanpur"
+placement="ho jayegi"
+console.table([id,name,age,course,city,placement])
