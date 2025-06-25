b629647815dbda30f58f21330aa1e96201a9c24d
Patel2080 <146932559+Patel2080@users.noreply.github.com>
2025-06-25 05:12:34 +0000

    date done

---
+++ 01_basics/07_date.js
@@ -0,0 +1,17 @@
+
+const date = new Date();
+
+console.log("toString():", date.toString()); 
+console.log("toDateString():", date.toDateString()); 
+console.log("toTimeString():", date.toTimeString()); 
+console.log("Symbol.toPrimitive (string):", date[Symbol.toPrimitive]("string")); 
+
+console.log("toISOString():", date.toISOString()); 
+console.log("toJSON():", date.toJSON()); 
+console.log("toUTCString():", date.toUTCString()); 
+
+console.log("toLocaleString():", date.toLocaleString()); 
+console.log("toLocaleDateString():", date.toLocaleDateString()); 
+console.log("toLocaleTimeString():", date.toLocaleTimeString());
+const istTime = date.toLocaleString("en-IN", { timeZone: "Asia/Kolkata" });
+console.log("Indian Time:", istTime);
